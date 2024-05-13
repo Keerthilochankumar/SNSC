834c64593e97a863e11217824ad32a6983209f12
keerthilochankumar <keerthi.lochans@gmail.com>
2024-05-13 19:46:00 +0530

    Refreshed the Sign-In and Registration Pages, Introduced a New Homepage Design

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,24 @@
 import { createBrowserRouter } from "react-router-dom";
 import Signin from "../pages/sigin";
+import SignupForm from "../pages/signup";
+import Hello from"../pages/index/Hello";
+
+
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello</div>,
+    element: <Hello/>,
   },
   {
     path: "/signin",
     element: <Signin />,
   },
-
+  {
+    path: "/signup",
+    element: <SignupForm/>,
+  },
+  
 ]);
 
 export default router;
