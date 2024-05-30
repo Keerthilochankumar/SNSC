b30cbc5945286a0e903caffbf3afbff37d7d7495
keerthilochankumar <keerthi.lochans@gmail.com>
2024-05-30 19:48:45 +0530

    Added PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,53 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default ({ mode }) => {
   return defineConfig({
     build: {
       outDir: "build",
     },
-    plugins: [react()],
-    define: {
-      "process.env.NODE_ENV": `"${mode}"`,
-    },
+    plugins: [
+      react(),
+      VitePWA({
+        devOptions: {
+          enabled: true 
+        },
+        manifest: {
+          name: "News Application",
+          short_name: "SNSC",
+          icons: [
+            {
+              "src": "/favicon.ico",
+              "sizes": "64x64 32x32 24x24 16x16",
+              "type": "image/x-icon"
+            },
+            {
+              "src": "/favicon-16x16.png",
+              "type": "image/png",
+              "sizes": "16x16"
+            },
+            {
+              "src": "/favicon-32x32.png",
+              "type": "image/png",
+              "sizes": "32x32"
+            },
+            {
+              "src": "/pwa-192x192.png",
+              "type": "image/png",
+              "sizes": "192x192"
+            },
+            {
+              "src": "/pwa-512x512.png",
+              "type": "image/png",
+              "sizes": "512x512",
+              "purpose": "any maskable" 
+            }
+          ],
+          theme_color: '#AAF',
+        },
+      }),
+    ],
   });
 };
